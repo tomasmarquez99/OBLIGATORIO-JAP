f00e96cc578e6568c7251a0d1fd0ecfa0e6b097c
tomasmarquez99 <btomasmarquez@gmail.com>
2021-08-16 23:53:16 -0300

    Creacion del login, avance en json

--- js/products.js
+++ js/products.js
@@ -1,7 +1,36 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+function getJSONData(PRODUCTS_URL) {
+    var contenedor = document.getElementsByClassName("producto");
     
+    fetch(PRODUCTS_URL)
+        .then(respuesta => respuesta.json())
 
-});
+        .then(datos => {
+            for (let i in datos){
+                var datosJSON = datos[i].name;
+                console.log(datosJSON);
+                document.createTextNode(datosJSON);
+parrafo = document.createElement('p');
+document.body.appendChild(parrafo);
+parrafo.appendChild(datosJSON);
+
+
+
+           
+              
+                
+            };
+            
+      
+
+
+        })
+        .catch(error => alert("Hubo un error: " + error));
+};
+document.addEventListener("DOMContentLoaded", getJSONData(PRODUCTS_URL));
+
+
+
+
