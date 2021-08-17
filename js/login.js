5e54dcda5e58c7d044b79d0f5e38399b48bf348b
tomasmarquez99 <btomasmarquez@gmail.com>
2021-08-17 00:01:14 -0300

    agregar login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,19 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
+=======
+
+/*document.addEventListener("DOMContentLoaded",);*/ 
+
+
+
+function boton(){
+
+window.location.href = "index.html"
+
+};
+>>>>>>> Tomas
