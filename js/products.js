b47eb6caf77c285190aaa6ffbbb2b4533086f51a
tomasmarquez99 <btomasmarquez@gmail.com>
2021-08-19 08:28:44 -0300

    Index en login

--- js/products.js
+++ js/products.js
@@ -1,29 +1,29 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-var arrayProductos = [];
+
+
+var productosArray = [];
+
+function mostrarProductos(array) {
+
+let articulos = '<br><hr><br>';
+  for (let i = 0; i < array.lenght; ++i){
+   let productos = array[i];
   
+  contenido += `Precio:` + array.price + `<br>;`
     
-function arrayProductos(array) {
-
-            for (let i = 0; i < array.length; ++i) {
-                console.log(array[i].name);
-                
-
-
-           
-              
-                
-            };
-        };
-            
-      
-
+    
+  };
+  document.getElementById("productos").innerHTML = articulos;
+};
 
 document.addEventListener("DOMContentLoaded", function (e){
-getJSONData(PRODUCTS_URL).then(function (resultado) {
-
-
-})}
-
-);
+  getJSONData(PRODUCTS_URL).then(function (resultado){ 
+    if (resultado.status === "ok"){
+      productosArray = resultado.data;
+      
+      mostrarProductos(productosArray);
+    }
+  })
+})
