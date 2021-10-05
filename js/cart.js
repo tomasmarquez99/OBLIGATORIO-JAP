d26bc3bcf2483b4581460b654d676a692be66b33
tomasmarquez99 <btomasmarquez@gmail.com>
2021-10-05 10:43:50 -0300

    Suma de los dos json

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,49 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
-});
+function mostrarCarrito(array) {
+    let htmlContentToAppend = '';
+    for (let i = 0; i < array.length; i++) {
+        producto = array[i]
+
+        htmlContentToAppend += `
+        <hr class="my-3">
+        <h4 id="productName">${producto.name}</h3>
+        <img src="${producto.src}">
+        <dl>
+        
+        <dd>
+        <br>
+        <p id="precio">Precio:${producto.currency} ${producto.unitCost}</p>
+        </dd>
+  
+        <dt>Cantidad de productos</dt>
+        <dd>
+        <p id="productCount">${producto.count}</p>
+        </dd>
+    
+    
+    `
+    document.getElementById('carrito').innerHTML = htmlContentToAppend
+
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+
+    getJSONData('https://japdevdep.github.io/ecommerce-api/cart/654.json')
+        .then(function (resultObj) {
+            if (resultObj.status === "ok") {
+
+                productoCarrito = resultObj.data;
+
+                
+                mostrarCarrito(productoCarrito.articles);
+
+
+            }
+
+        });
+})
